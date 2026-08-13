c6ad79c3cc2921a7e4679aec4385fc486c6ebabd
kuroi042 <bouderraoui45@gmail.com>
2026-08-13 12:48:31 +0100

    print

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import time
 from ultralytics import YOLO
 path =  "/home/kenshin/Desktop/DataProject/dvr/vid.mp4"
 model  =  YOLO("yolo11n.pt")
-
+class_name = model.names #* return a dictionary of yolo class index 
+#* boxes.xyxy → WHERE
+#* boxes.conf → HOW CONFIDENT
+#* boxes.cls  → WHAT CLASS
 
 
 
@@ -17,26 +20,25 @@ width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 fourcc =  cv2.VideoWriter_fourcc(*'mp4v')
-
+current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
 out = cv2.VideoWriter('out.mp4',
                 fourcc,
                 fps,(width,height) )
-
 while True:
     ret, frame =  cap.read()
     if not ret :
         break
 
-    newtime  =  time.time()
-    dif_time  =  newtime- prevtime
-    fps  =  1/dif_time if dif_time >0 else 0
-    prevtime =  newtime
     fps_text =  f"FPS : {int(fps)}"
     cv2.putText(frame, fps_text, (10,40),
                 cv2.FONT_HERSHEY_SIMPLEX,1,
                  (200,0,0,),2,cv2.LINE_AA)
-    print(f"fps:{fps}| width:{width}| Height:{height}| frame_count:{frame_count}")
-    result =  model(frame, classes=0)
+
+    result =  model.track(frame,persist=True) #*on the current frame
+    for c  in result[0].boxes:
+        index = int(c)
+        classid =  
+
     new_frame =  result[0].plot()
 
     cv2.imshow('recording ... ',new_frame)
