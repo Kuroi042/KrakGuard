b5f8b53e063e9d81d1d87dacac5524376300dc4b
kuroi042 <bouderraoui45@gmail.com>
2026-08-14 19:49:37 +0100

    Save

--- count.py
+++ count.py
@@ -2,84 +2,83 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 model  =  YOLO("yolo11s.pt")
-cap =  cv2.VideoCapture('traf.mp4')
+cap =  cv2.VideoCapture('source/traffic.mp4')
 fps  =  cap.get(cv2.CAP_PROP_FPS)
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+TARGET_WIDTH = 1280
+TARGET_HEIGHT = 720
 trajectorie = {}
 previous_side = {}
 inn, out = 0,0
-cv2.namedWindow('img')
+line_y = 280
+line_top = 630
+line_bottom = 660
 while(True):
-    print(width, height)
-# 22 in | out 23-24
     ret, frame = cap.read() 
+    frame = cv2.resize(frame, (TARGET_WIDTH, TARGET_HEIGHT))
+
     fps_text =  f"FPS: {int(fps)}"
-    # cv2.putText(`frame, fps_text,(10,40), cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA)
-    result =  model.track(source=frame, persist=True,conf=0.1)
-    line_y = 280
-    line_top = 260
-    line_bottom = 280
-    # cv2.line(frame, (0,line_y),(int(width),line_y), (255, 0, 0),2,2)
-    cv2.rectangle(frame, (0,260),(int(width),300),(225,225,0),2,cv2.LINE_AA)
-    for i in (result[0].boxes):
-        x1,y1,x2,y2 =  np.array(i.xyxy[0])
-        centerx =  int((x1+x2)/2)
-        centery =  int((y1+y2)/2)
 
-        point_x = int((x1 + x2) / 2)
-        point_y = int(y2)
+    result =  model.track(source=frame,persist=True,conf=0.25)
 
-        track_id =  int(i.id[0])
-        if i.id is not None:
-            track_id = int(i.id[0])
-        else:
-            track_id = None
-        if track_id not in trajectorie:
-            trajectorie[track_id]=[]
-        trajectorie[track_id].append((centerx,centery))
-        for id , point in trajectorie.items():
-            #* point = [(centerx, centery)]
+    cv2.rectangle(frame, (0,630),(int(TARGET_WIDTH),660),(225,225,0),-1,cv2.LINE_AA)
+    boxes =  result[0].boxes
+    if boxes is not None and len(boxes)>0:
+        for i in boxes:
+            x1,y1,x2,y2 =  np.array(i.xyxy[0])#bbox
+            centerx =  int((x1+x2)/2)
+            centery =  int((y1+y2)/2)
+
+            point_x = int((x1 + x2)/2)  
+            point_y = int(y2) 
+
+            if i.id is not None:
+                track_id = int(i.id[0]) 
+            else:
+                continue
+
+            if track_id not in trajectorie:
+                trajectorie[track_id]=[] #create an empty list for id
+            trajectorie[track_id].append((centerx,centery))#fill the list
+            point = trajectorie[track_id]
             for j in range(1, len(point)):
+                    cv2.line(frame, (point[j-1]), (point[j]) ,(0,225,0),1,cv2.LINE_AA)
 
-                cv2.line(frame, (point[j-1]), (point[j]) ,(0,225,0),1,cv2.LINE_AA)
+            if point_y < line_top: #
+                current_side = "above"
+            elif point_y >line_bottom:
+                current_side = "below"
+            else:
+                current_side = "zone"
+            
+            if track_id in previous_side:
+                old_side = previous_side[track_id]
 
-        # current_side = "above" if centery < line_top else "below" #*centry >liney
-        if point_y < line_top:
-            current_side = "above"
-        elif point_y >line_bottom:
-            current_side = "below"
-        else:
-            current_side = "zone"
+                # ABOVE → ZONE → BELOW
+                if old_side == "zone" and current_side == "below":
+                    if previous_side.get(f"{track_id}_start") == "above":#history checl
+                        out += 1
+                        print(f"ID {track_id} → OUT")
+
+                # BELOW → ZONE → ABOVE
+                elif old_side == "zone" and current_side == "above":
+                    if previous_side.get(f"{track_id}_start") == "below":
+                        inn += 1
+                        print(f"ID {track_id} → IN")
+
+                # Remember where the object entered the zone
+                if old_side != "zone" and current_side == "zone":
+                    previous_side[f"{track_id}_start"] = old_side
+                
+            previous_side[track_id] = current_side
         
-        if track_id in previous_side:
-
-            old_side = previous_side[track_id]
-
-            # ABOVE → ZONE → BELOW
-            if old_side == "zone" and current_side == "below":
-                if previous_side.get(f"{track_id}_start") == "above":
-                    out += 1
-                    print(f"ID {track_id} → OUT")
-
-            # BELOW → ZONE → ABOVE
-            elif old_side == "zone" and current_side == "above":
-                if previous_side.get(f"{track_id}_start") == "below":
-                    inn += 1
-                    print(f"ID {track_id} → IN")
-
-            # Remember where the object entered the zone
-            if old_side != "zone" and current_side == "zone":
-                previous_side[f"{track_id}_start"] = old_side
-
-        previous_side[track_id] = current_side
-        
-        text = f"Out:{out}--------------in{inn}"
-        cv2.putText(frame,text, (50,40),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),1,cv2.LINE_AA)
+        text = f"Out:{out}----in{inn}"
+        cv2.putText(frame,text, (50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),1,cv2.LINE_AA)
 
 
-    vid  =  result[0].plot()
-    cv2.imshow('img',vid)
+    # cv2.imshow("Vehicle Counter 
+    # vid  =  result[0].plot()
+
+    cv2.imshow('img',frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 cap.release()
