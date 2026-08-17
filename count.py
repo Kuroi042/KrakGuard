3865d6605d46f6a09d42a854afb620984d1fa583
kuroi042 <bouderraoui45@gmail.com>
2026-08-17 02:06:51 +0100

    save for backup , ill refactor the function

--- count.py
+++ count.py
@@ -26,12 +26,7 @@ while True:
     result = model.track(frame, persist=True,classes=[2, 3, 7, 5], conf=0.25,imgsz=640)
     cv2.rectangle(frame, (0,bar[0]),(WIDTH,bar[1]),(225,0,0),1,cv2.LINE_4)
     seen_now =  set()
-    for trackid in list(last_seen.keys()):
-        if trackid not in seen_now:
-            last_seen[trackid][2] += 1
 
-            if last_seen[trackid][2]>GRACE_FRAMES:
-                del last_seen[trackid]
     for box in result[0].boxes:
 
         if box.id is None:
@@ -40,10 +35,9 @@ while True:
         centerx = int((x1+x2)/2)
         centery =  int(y2)
         trackid =  int(box.id[0])
+
         seen_now.add(trackid)
-        # print("Seennow" , seen_now[box.id])
         cv2.rectangle(frame, (x1,y1),(x2,y2),(0,0,225),1, cv2.LDR_SIZE)
-        # cv2.circle(frame, (centerx, centery),10,(0,0,255),-1)
         if centery < bar[0]:
             current ="above"
         elif centery > bar[1]:
@@ -53,14 +47,6 @@ while True:
         previous =  side_of_line.get(trackid) #get the value for specific trackid
  
         side_of_line[trackid]=current #this one first then previous
-
-        # print(side_of_line.items())
-        # if current == "above":
-        #     print(f" {int(box.id)} {centery} ->  {current}")
-        # elif     current ==  "below":
-        #     print(f"{int(box.id)} {centery}-> {current}")
-        # elif     current ==  "zone":
-        #     print(f" {int(box.id)} :  { centery}-> {current}")
         if previous=="above" and current =="zone":
              entered_from[trackid]="above"
 
@@ -84,15 +70,15 @@ while True:
         cv2.circle(frame, (centerx, centery), 10, color, -1)
 
         text_out = f"in: {inn}----out: {out}"
-        
+       
         cv2.putText(frame, text_out, (50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(225,0,0),1,cv2.LINE_AA)
         last_seen[trackid]=[centerx, centery,0] #zero for zero frame been missing 
-        # #counter reset to 0 each time the car is detected
-        # for trackid in last_seen:
-        #     x,y,missing = last_seen[trackid]
-            # if missing>0:
-            #         cv2.circle(frame, (x,y),10,(0,225,225),1)
-            
+    for trackid in list(last_seen.keys()):
+        if trackid not in seen_now:
+            last_seen[trackid][2] += 1
+
+            if last_seen[trackid][2]>GRACE_FRAMES:
+                del last_seen[trackid]
     cv2.imshow("img", frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
