4ad22949aa0fffa215d8d474b129ecbddfcd0cdc
kuroi042 <bouderraoui45@gmail.com>
2026-08-17 02:54:03 +0100

    main.py , count.py

---
+++ counting.py
@@ -0,0 +1,77 @@
+
+# model  =YOLO("yolo11n.pt")
+# cap = cv2.VideoCapture("source/traf.mp4")
+WIDTH =  1280
+import cv2
+from ultralytics import YOLO
+HEIGHT =720   
+GRACE_FRAMES=10      #above
+bar=[500,600]       #----- 500
+                    #----- 600
+                    #below
+side_of_line={} #id+side
+is_in={}
+entered_from={}
+crossed = {}
+last_seen = {} #keep track of ID 
+
+previous = None
+col=[(0,225,0),(0,0,255)]
+def count(result, frame, inn, out):
+    cv2.rectangle(frame, (0,bar[0]),(WIDTH,bar[1]),(225,0,0),5,cv2.LINE_4)
+    seen_now =  set()
+
+    for box in result[0].boxes:
+
+            if box.id is None:
+                continue
+            x1,y1,x2,y2 = map(int,box.xyxy[0])
+            centerx = int((x1+x2)/2)
+            centery =  int(y2)
+            trackid =  int(box.id[0])
+
+            seen_now.add(trackid)
+            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,0,225),1, cv2.LINE_AA)
+            if centery < bar[0]:
+                current ="above"
+            elif centery > bar[1]:
+                current ="below"
+            else:
+                current = "zone"
+            previous =  side_of_line.get(trackid) #get the value for specific trackid
+    
+            side_of_line[trackid]=current #this one first then previous
+            if previous=="above" and current =="zone":
+                entered_from[trackid]="above"
+
+            elif previous=="below" and current == "zone":
+                entered_from[trackid]="below"
+
+            if previous == "zone" and current =="below" and entered_from.get(trackid)=="above":
+                if not crossed.get(trackid, False):
+                    out+=1
+                    crossed[trackid]="out"
+            if previous ==  "zone" and current =="above" and entered_from.get(trackid)=="below":
+                if not crossed.get(trackid, False):
+                    inn+=1
+                    crossed[trackid]="in"
+            if crossed.get(trackid)=="in":
+                color = col[0]        
+            elif crossed.get(trackid)=="out":
+                color = col[1]
+            else:
+                color = (225,225,225)
+            cv2.circle(frame, (centerx, centery), 10, color, -1)
+
+            text_out = f"in: {inn}----out: {out}"
+        
+            cv2.putText(frame, text_out, (50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(225,0,0),1,cv2.LINE_AA)
+            last_seen[trackid]=[centerx, centery,0] #zero for zero frame been missing 
+    for trackid in list(last_seen.keys()):
+            if trackid not in seen_now:
+                last_seen[trackid][2] += 1
+
+                if last_seen[trackid][2]>GRACE_FRAMES:
+                    del last_seen[trackid]
+
+    return frame ,inn, out
--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from ultralytics import YOLO
-import numpy as np
 import cv2
-import os
+import numpy as np
+from counting import count
+model  =YOLO("yolo11n.pt")
+WIDTH =  1280
+HEIGHT =720 
+inn = 0
+out = 0
+def main():
 
-model_path='yolo11s.pt'
+    model = YOLO("yolo11n.pt")
+    cap = cv2.VideoCapture("source/traffic.mp4")
 
-class Detector:
-    def __init__(self, model):
-        self.model = YOLO(model_path)
-    def detect(self,frame):
-        return self.model(frame)
-class Tracker:
-    def __init__(self,model):
-        self.model =model
-    def track(self, frame):
-        return self.model.track(
-            frame, persist=True,
-            conf = 0.25
+    while True:
+
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        frame = cv2.resize(frame, (WIDTH, HEIGHT))
+
+        result = model.track(
+            frame,
+            persist=True,
+            classes=[2, 3, 7, 5],
+            conf=0.25,
+            imgsz=640
         )
-    
+        frame, inn, out = count(result, frame, 0, 0)  
+        cv2.imshow("img", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+if __name__ =="__main__":
+    main()
