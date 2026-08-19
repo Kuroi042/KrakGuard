fcc1136cec708d40250d0797ec043f59d2294f24
kuroi042 <bouderraoui45@gmail.com>
2026-08-20 00:26:42 +0100

    improvement

--- radar.py
+++ radar.py
@@ -7,46 +7,34 @@ WIDTH = 1280
 HEIGHT = 720
 class Radar: 
     def __init__(self): 
-        self.fps = 30   
-        self.positions={}
-        self.prevPosition={}
-        self.distance=None
-        self.meters_per_pixel = 0.05
-        self.time = float(1/self.fps)
+        self.position={}
+        self.fps= 30
+        self.previous_pos={}
+# get the center of each 
     def radarr(self,result):
         radar = np.zeros((RADAR_HEIGHT, RADAR_WIDTH, 3)
-                         , dtype=np.uint8)             
+                         , dtype=np.uint8)        #canvas for dots   
+        for i in result[0].boxes:
+            x1,y1,x2,y2  = map(int, i.xyxy[0])
+            centerx = int((x1+x2)/2)
+            centery =  int(y2) #bottom
+            trackid =  int(i.id[0])
+            self.previous_pos[trackid]= self.position.get(trackid)#get postion old values
+            self.position[trackid]= (centerx, centery)#update
 
-        for box in result[0].boxes:
-            if box.id is None:
-                continue
-            x1,y1,x2,y2 =  map(int,box.xyxy[0])
-            centerx=int((x1+x2)/2) 
-            centery = int(y2)  
-            trackid =  int(box.id[0])
-            radar_x =  int(centerx *RADAR_WIDTH / WIDTH) #get the middle
-            radar_y = int(centery * RADAR_HEIGHT / HEIGHT )#get the middle
-            cv2.circle(radar,(radar_x, radar_y),5,(0,0,225),-1)
-            previous = self.prevPosition[trackid]=self.positions.get(trackid)
-            current  = self.positions[trackid] = (centerx, centery) 
-            if previous is not None:
+            previous =  self.previous_pos[trackid]
+            current = self.position[trackid]
+            if previous is not None:    
+                distance =  np.linalg.norm(np.array(current) - np.array(previous))
+                print(trackid,distance)
+            # distance = self.current-self.previous
+            # print(trackid ,distance)
+            # centerx in width 
+            # Radar_x in RADAR_WIDTH
+            #la regle de 3
+            radar_x =  int((centerx*RADAR_WIDTH )/WIDTH) 
+            radar_y =  int((centery*RADAR_HEIGHT)/HEIGHT)
 
-                self.distance = np.linalg.norm(
-                    np.array(current) - np.array(previous)
-                )
-
-                meters_per_second = (
-                    self.distance
-                    * self.fps
-                    * self.meters_per_pixel
-                )
-
-                speed_kmh = meters_per_second * 3.6
-
-                self.speed_limit = 50
-
-                if speed_kmh > self.speed_limit:
-                    print(f"ID {trackid} -> SPEEDING: {speed_kmh:.1f} km/h")
-            # print(trackid, self.distance)
+            cv2.circle(radar, (radar_x, radar_y),5,(225,0,0),-1 )
 
         return radar
